2512456e2bb30ef44d6143cef836fb366e96c6e5
Victor <victorzevallos@protonmail.com>
2020-04-30 13:03:54 -0500

    Add login view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            print(user)
+            if user is not None:
+                if user.is_active:
+                    # Establece al usuario en la sesión
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disable account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
