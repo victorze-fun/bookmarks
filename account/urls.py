ba8720b3863e9b02f7b063d92a641fe020843692
Victor <victorzevallos@protonmail.com>
2020-05-01 21:08:53 -0500

    Add user profile

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -8,4 +10,9 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -125,3 +125,7 @@ LOGIN_URL = 'login' # si accede a una página protegida y no esta authenticado
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# development server
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
